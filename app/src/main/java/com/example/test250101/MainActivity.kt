65adf1a5d81be36e63d8fbde8774d63173af88e9
류혜선 <LG@192.168.120.24>
2025-03-15 01:01:04 +0900

    03-15

--- app/src/main/java/com/example/test250101/MainActivity.kt
+++ app/src/main/java/com/example/test250101/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+// MainActivity : 알고리즘 작성
+// activity_main.xml : layout 설정
 class MainActivity : AppCompatActivity() {
+
+    // 생성 시 호출되는 fun
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
+
 }
